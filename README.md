c4b4c4e7cfc9ae3aa32222cac27a4ca5277f9200
Nicholas Treu <nicholas.treu@autostoresystem.com>
2021-11-30 23:38:35 -0700

    Solution for Day 1.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Advent-of-Code-2021
+# Advent of Code 2021
 My solutions to Advent of Code 2021
---
+++ day01/Program.fs
@@ -0,0 +1,32 @@
+﻿open System.IO
+
+let findIncreases (total, last) current =
+    if current > last then
+        (total + 1, current)
+    else
+        (total, current)
+
+let findIncreaseSums (total, last) xs =
+    let currentSum = Seq.sum xs
+
+    if currentSum > last then
+        (total + 1, currentSum)
+    else
+        (total, currentSum)
+
+let input = 
+    File.ReadAllLines "input.txt" |> Seq.map int
+
+let part1 =
+    input
+    |> Seq.fold findIncreases (0, 150) // 150 is my first input value
+    |> fst
+
+let part2 =
+    input 
+    |> Seq.windowed 3
+    |> Seq.fold findIncreaseSums (0, Seq.take 3 input |> Seq.sum) // Need to start with the sum of the first window
+    |> fst
+
+printfn "%i" part1
+printfn "%i" part2
