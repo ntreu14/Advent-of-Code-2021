974eadc07223b9cb1cad922fdbd952486b99924c
Nicholas Treu <nicholas.treu@autostoresystem.com>
2021-12-08 16:46:26 -0700

    Use 2 instead of 4 spaces in F# because it like it better.

--- day01/Program.fs
+++ day01/Program.fs
@@ -1,21 +1,21 @@
 ﻿open System.IO
 
 let input = 
-    File.ReadAllLines "input.txt" |> Seq.map int
+  File.ReadAllLines "input.txt" |> Seq.map int
 
 let findCountOfIncreases xs =
-    Seq.pairwise xs
-    |> Seq.filter (fun (previous, current) -> current > previous)
-    |> Seq.length
+  Seq.pairwise xs
+  |> Seq.filter (fun (previous, current) -> current > previous)
+  |> Seq.length
 
 let part1 =
-    findCountOfIncreases input
- 
+  findCountOfIncreases input
+
 let part2 =
-    input 
-    |> Seq.windowed 3
-    |> Seq.map Seq.sum
-    |> findCountOfIncreases
+  input 
+  |> Seq.windowed 3
+  |> Seq.map Seq.sum
+  |> findCountOfIncreases
 
 printfn "%i" part1
 printfn "%i" part2
--- day08/Program.fs
+++ day08/Program.fs
@@ -5,11 +5,11 @@ let input = File.ReadAllLines "input.txt"
 
 // Part 1
 let toOnlyKnownSegments soughtCounts (line: string) =
-    let isSoughtCount (output: string) = Seq.contains output.Length soughtCounts
+  let isSoughtCount (output: string) = Seq.contains output.Length soughtCounts
 
-    match splitOnWithoutEmpties line '|' with
-    | [| _; outputs |] -> splitOnWithoutEmpties outputs ' ' |> Seq.filter isSoughtCount
-    | other -> failwith $"Cannot parse from part 1: {other}, length: {Seq.length other}"
+  match splitOnWithoutEmpties line '|' with
+  | [| _; outputs |] -> splitOnWithoutEmpties outputs ' ' |> Seq.filter isSoughtCount
+  | other -> failwith $"Cannot parse from part 1: {other}, length: {Seq.length other}"
 
 Seq.map (toOnlyKnownSegments [2; 4; 3; 7]) input 
 |> Seq.sumBy Seq.length
@@ -18,76 +18,76 @@ Seq.map (toOnlyKnownSegments [2; 4; 3; 7]) input
 
 // Part 2
 let knownLengthToSegmentLookup segments knownLengths = // length => known segment
-    segments
-    |> Seq.map (fun (segment: string) -> (segment.Length, set segment))
-    |> Seq.filter (fun (length, _) -> Seq.contains length knownLengths)
-    |> Map.ofSeq
+  segments
+  |> Seq.map (fun (segment: string) -> (segment.Length, set segment))
+  |> Seq.filter (fun (length, _) -> Seq.contains length knownLengths)
+  |> Map.ofSeq
 
 let knownLengthToDigitLookup = // length => known digit
-    Map.ofList [(2, 1); (4, 4); (3, 7); (7, 8)]
+  Map.ofList [(2, 1); (4, 4); (3, 7); (7, 8)]
 
 let parseSignalPatternsAndOutputs (line: string) =
-    match splitOnWithoutEmpties line '|' with
-    | [| signalPatterns; outputs |] ->
-        (splitOnWithoutEmpties signalPatterns ' ', splitOnWithoutEmpties outputs ' ') 
+  match splitOnWithoutEmpties line '|' with
+  | [| signalPatterns; outputs |] ->
+      (splitOnWithoutEmpties signalPatterns ' ', splitOnWithoutEmpties outputs ' ') 
 
-    | other ->
-        failwith $"Cannot parse: {other}, length: {Seq.length other}"
+  | other ->
+      failwith $"Cannot parse: {other}, length: {Seq.length other}"
 
 let isIntersectionCountOf segment intersectioncount knownDigitSegment =
-    Set.intersect (set segment) knownDigitSegment
-    |> Set.count 
-    |> (=) intersectioncount
+  Set.intersect (set segment) knownDigitSegment
+  |> Set.count 
+  |> (=) intersectioncount
 
 let decodeSignals knownLengthToSegment decodedLookup (segment: string) =
-    match segment.Length with
+  match segment.Length with
 
-    // Known segements by length
-    | segmentLength when Seq.contains segmentLength <| Map.keys knownLengthToDigitLookup ->
-        let digit = Map.find segmentLength knownLengthToDigitLookup
-        Map.add (set segment) digit decodedLookup
+  // Known segements by length
+  | segmentLength when Seq.contains segmentLength <| Map.keys knownLengthToDigitLookup ->
+      let digit = Map.find segmentLength knownLengthToDigitLookup
+      Map.add (set segment) digit decodedLookup
 
-    // Length 5
-    | 5 ->
-        // We know 3 because it has 2 intersections with 1
-        if Map.find 2 knownLengthToSegment |> isIntersectionCountOf segment 2 then
-            Map.add (set segment) 3 decodedLookup
-        
-        // We know 5 because it has 3 intersections with 4
-        elif Map.find 4 knownLengthToSegment |> isIntersectionCountOf segment 3 then
-            Map.add (set segment) 5 decodedLookup
+  // Length 5
+  | 5 ->
+      // We know 3 because it has 2 intersections with 1
+      if Map.find 2 knownLengthToSegment |> isIntersectionCountOf segment 2 then
+          Map.add (set segment) 3 decodedLookup
+      
+      // We know 5 because it has 3 intersections with 4
+      elif Map.find 4 knownLengthToSegment |> isIntersectionCountOf segment 3 then
+          Map.add (set segment) 5 decodedLookup
 
-        // Otherwise 2 is the only remaining digit with length 5
-        else 
-            Map.add (set segment) 2 decodedLookup
+      // Otherwise 2 is the only remaining digit with length 5
+      else 
+          Map.add (set segment) 2 decodedLookup
 
-    // Length 6 
-    | 6 ->
-        // We know 9 because it has 4 intersections with 4
-        if Map.find 4 knownLengthToSegment |> isIntersectionCountOf segment 4 then
-            Map.add (set segment) 9 decodedLookup
+  // Length 6 
+  | 6 ->
+      // We know 9 because it has 4 intersections with 4
+      if Map.find 4 knownLengthToSegment |> isIntersectionCountOf segment 4 then
+          Map.add (set segment) 9 decodedLookup
 
-        // We know 0 because it has 2 intersections with 1
-        elif Map.find 2 knownLengthToSegment |> isIntersectionCountOf segment 2 then
-            Map.add (set segment) 0 decodedLookup
+      // We know 0 because it has 2 intersections with 1
+      elif Map.find 2 knownLengthToSegment |> isIntersectionCountOf segment 2 then
+          Map.add (set segment) 0 decodedLookup
 
-        // Otherwise 6 is the only remaining digit with length 6
-        else 
-            Map.add (set segment) 6 decodedLookup
+      // Otherwise 6 is the only remaining digit with length 6
+      else 
+          Map.add (set segment) 6 decodedLookup
 
-    // Truly an exceptional case, we did something very wrong to get here...
-    | otherLength -> failwith $"Some other length {otherLength} of {segment}"
+  // Truly an exceptional case, we did something very wrong to get here...
+  | otherLength -> failwith $"Some other length {otherLength} of {segment}"
 
 let decodeSegment (wholeLine: string) =
-    let segments, outputs = parseSignalPatternsAndOutputs wholeLine 
-    let knownLengthToSegment = knownLengthToSegmentLookup segments [2; 4; 3; 7]
-    let decodedValues = segments |> Seq.fold (decodeSignals knownLengthToSegment) Map.empty
+  let segments, outputs = parseSignalPatternsAndOutputs wholeLine 
+  let knownLengthToSegment = knownLengthToSegmentLookup segments [2; 4; 3; 7]
+  let decodedValues = segments |> Seq.fold (decodeSignals knownLengthToSegment) Map.empty
 
-    // Find the output value from the map we just made, combine it's digits, and convert it to an int
-    outputs 
-    |> Seq.map (fun output -> Map.find (set output) decodedValues |> string) 
-    |> String.concat ""
-    |> int
+  // Find the output value from the map we just made, combine it's digits, and convert it to an int
+  outputs 
+  |> Seq.map (fun output -> Map.find (set output) decodedValues |> string) 
+  |> String.concat ""
+  |> int
 
 input
 |> Seq.sumBy decodeSegment
